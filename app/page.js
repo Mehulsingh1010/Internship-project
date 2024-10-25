c1a847061bac6c43ca16f16973a6aceb4eceb985
kunalArya1 <kunalkrraj220@gmail.com>
2024-10-25 10:29:48 +0530

    ALl Section of home page added

--- app/page.js
+++ app/page.js
@@ -1,7 +1,17 @@
+import CaseStudies from "@/components/caseStudies";
+import DesignApproachSection from "@/components/designApproachData";
+import FeaturedResources from "@/components/FeaturedResources";
+import Footer from "@/components/Footer";
 import HeroSection from "@/components/HeroSection";
 import Navbar from "@/components/Navbar";
 import PartnerSection from "@/components/PartnerSection";
+import PromoBanner from "@/components/PromoBanner";
+import ReviewSlider from "@/components/ReviewSlider";
+import SoftwareBuildingSection from "@/components/SoftwareBuildingSection";
 import SoftwareSection from "@/components/SoftwareSection";
+import TechStackSection from "@/components/techStackData";
+import TestimonialSlider from "@/components/TestimonialSlider";
+import Timeline from "@/components/Timeline";
 import Image from "next/image";
 
 export default function Home() {
@@ -11,6 +21,16 @@ export default function Home() {
       <HeroSection />
       <SoftwareSection />
       <PartnerSection />
+      <TestimonialSlider />
+      <CaseStudies />
+      <SoftwareBuildingSection />
+      <DesignApproachSection />
+      <TechStackSection />
+      <Timeline />
+      <FeaturedResources />
+      <ReviewSlider />
+      <PromoBanner />
+      <Footer />
     </div>
   );
 }
