57da6c9b82aa9167f5dc995e53a7b2fc455a7e71
kunalArya1 <kunalkrraj220@gmail.com>
2024-10-25 10:41:00 +0530

    Bug Fixed

--- app/page.js
+++ app/page.js
@@ -16,7 +16,7 @@ import Image from "next/image";
 
 export default function Home() {
   return (
-    <div>
+    <div className=" overflow-hidden">
       <Navbar />
       <HeroSection />
       <SoftwareSection />
